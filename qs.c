f10cbb198a8a08d687d50bae6df661834d1a931a
Phatcharamon Namwayko <pnamwayk@z2t7c4.42bangkok.com>
2023-04-11 16:09:08 +0700

    sorted

--- qs.c
+++ qs.c
@@ -40,8 +40,6 @@ int qs_pb(t_list **lst, t_stc *stack, int size)
 	find_min_max_idex(stack->a, stack, size);
 
 	stack->chunk_size = ((size / 2) + (size % 2));
-	stack->med = stack->min + stack->chunk_size - (stack->chunk_size % 2);
-	stack->lower = stack->min;
 	stack->upper = stack->min + stack->chunk_size - 1;
 	stack->pb_cnt = 0;
 	stack->ra_cnt = 0;
@@ -69,14 +67,13 @@ int qs_pb(t_list **lst, t_stc *stack, int size)
     }
 	// if (stack->size_a <= 3)
 	// 	return (0);
-	while(stack->pb_cnt != stack->chunk_size && size--)
+	while(stack->pb_cnt != stack->chunk_size)
     {
 		if (stack->a->id <= stack->upper)
         {
 			printf("a = %d | <= | up = %d |\n", stack->a->id , stack->upper);
 			stack->a->chunk_id = id1;
             pb(stack);
-			// stack->a = stack->a->next;
             stack->pb_cnt++;
         }
 		// else if (stack->a->next->id < stack->a->id && stack->size_b >= 2 && stack->b->next->val > stack->b->val)
@@ -90,11 +87,11 @@ int qs_pb(t_list **lst, t_stc *stack, int size)
 	while (stack->ra_cnt-- && sz != stack->chunk_size)
 		rra(stack);
 	// id++;
-	qs_pb(lst, stack, sz/2);
-	// qs_pa(lst, stack, sz/2 + sz%2);
+	qs_pb(lst, stack, sz - stack->chunk_size);
+	qs_pa(lst, stack, sz/2 + sz%2);
 	printf("finish---+___ sz = %d | size_a = %d | chunk_size = %d|\n", sz, stack->size_a, stack->chunk_size);
 	printf("finish---+___ sz/2 + sz2 = %d|\n", sz/2 + sz%2); 
-	qs_pa(lst, stack, sz/2 + sz%2);
+	// qs_pa(lst, stack, sz/2 + sz%2);
 	return (0);
 }
 
@@ -112,13 +109,13 @@ int qs_pa(t_list **lst, t_stc *stack, int size)
 	stack->pa_cnt = 0;
 	stack->ra_cnt = 0;
 	stack->rb_cnt = 0;
-	stack->chunk_size = (size / 2) + (size % 2);
+	stack->chunk_size = (size / 2);
 	stack->med = stack->min + stack->chunk_size - (stack->chunk_size % 2);
 	stack->lower = stack->min;
-	stack->upper = stack->max - stack->chunk_size + 1;
+	stack->upper = stack->min + (size / 2) + (size % 2) - 1;
 	id++;	
-	if (check_sort(stack->a) == 0 && stack->size_b == 0)
-		return (0);
+	// if (check_sort(stack->a) == 0 && stack->size_b == 0)
+	// 	return (0);
     if (size <= 3)
     {
 		if (size == 2 && stack->b->id < stack->b->next->id)
@@ -127,18 +124,18 @@ int qs_pa(t_list **lst, t_stc *stack, int size)
 			sort_3b_bottom(stack);
 		else if (size == 3)
 			sort_three_max(stack);
-		while (size-- && stack->size_b > 3)
+		while (size--)
 		{
 			printf("b = %d | sz = %d | up = %d |\n", stack->b->id, size, stack->upper);
 			pa(stack);
 		}
         return (id);
     }
-	if (stack->size_a <= 3)
+	if (stack_size(stack->b, stack) <= 3)
 		return (0);
-	while(stack->pa_cnt != stack->chunk_size && size--)
+	while(stack->pa_cnt != stack->chunk_size)
     {
-		if (stack->b->id >= stack->upper)
+		if (stack->b->id > stack->upper)
         {
 			printf("b = %d | >= | up = %d | size = %d\n", stack->b->id , stack->upper, size);
             pa(stack);
@@ -153,12 +150,12 @@ int qs_pa(t_list **lst, t_stc *stack, int size)
 	{
 			rrb(stack);
 	}
-	qs_pb(lst, stack, sz/2 + sz%2);
+	qs_pb(lst, stack, stack->chunk_size);
 	printf("finish_a1---+___ sz = %d | size_a = %d | chunk_size = %d|\n", sz, stack->size_a, stack->chunk_size);
 	printf("finish_a1---+___ sz/2 + sz2 = %d| >= up = %d\n", sz/2 + sz%2, stack->upper); 
-	qs_pa(lst, stack, sz/2);
-		printf("finish_a2---+___ sz = %d | size_a = %d | chunk_size = %d|\n", sz, stack->size_a, stack->chunk_size);
-	printf("finish_a2---+___ sz/2 + sz2 = %d|\n", sz/2 + sz%2);
+	qs_pa(lst, stack, sz/2  + sz%2);
+	// printf("finish_a2---+___ sz = %d | size_a = %d | chunk_size = %d|\n", sz, stack->size_a, stack->chunk_size);
+	// printf("finish_a2---+___ sz/2 + sz2 = %d|\n", sz/2 + sz%2);
 	(void) lst; 
 	return (0);
 }
@@ -211,8 +208,8 @@ int qs_pb2(t_list **lst, t_stc *stack, int size)
     }
 	while (stack->ra_cnt-- > 0 && sz != stack->chunk_size)
 		rra(stack);
-	// if(stack->chunk_size <= 3)
-	// 	sort_three_min(stack);
+	if(stack->chunk_size <= 3)
+		sort_three_min(stack);
 	printf("finish_b1---+___ sz = %d | size_a = %d | chunk_size = %d|\n", sz, stack->size_a, stack->chunk_size);
 	printf("finish_b1---+___ sz/2 + sz2 = %d|\n", sz/2 + sz%2); 
 	qs_pb2(lst, stack,  sz/2 );
